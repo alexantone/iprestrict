c29cea6d3a3b44d2d310036b7c8ce59b4d4ee696
alex.antone <alex.antone@47f1c0aa-a667-11dd-9bad-6bc74a82fd44>
2008-10-30 17:52:09 +0000

    Fixed parsing functions

--- iprestrict.c
+++ iprestrict.c
@@ -1,16 +1,15 @@
 /*
- * ---------------------------------------------------------------
+ * --------------------------------------------------------------------
  * iprestrict.c
  *
  *  Created on: Oct 24, 2008
  *      Author: sorin
- * ---------------------------------------------------------------
+ * --------------------------------------------------------------------
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 
 #include "iprestrict.h"
 #include "parse.h"
@@ -18,59 +17,61 @@
 static FILE *fh = NULL;
 static char *ifdev = NULL;
 
-int parse_args (const int argc,char * const argv[]) {
+int parse_args (const int argc, char * const argv[]) {
 
-	int ix;
+    int ix;
 
-	for (ix = 1; ix < argc; ) {
+    for (ix = 1; ix < argc;) {
 
-		if (strcmp(argv[ix], "-f") == 0 && fh == NULL) {
-			/*
-			 * Parse the filename
-			 */
-			ix++;
+        if (strcmp(argv[ix], "-f") == 0 && fh == NULL) {
+            /*
+             * Parse the filename
+             */
+            ix++;
 
-			fh = fopen(argv[ix], "r" );
+            fh = fopen(argv[ix], "r");
 
-			if (fh == NULL) {
-				fprintf(stderr, "File %s could not be opened!!!\n", argv[ix] );
-				return -1;
-			}
-			ix++;
-		}
-		else if (strcmp(argv[ix], "-i") == 0 && ifdev == NULL) {
-			/*
-			 * Parse the interface
-			 */
-			ix++;
-			ifdev = argv[ix];
-			ix++;
+            if (fh == NULL) {
+                fprintf(stderr,
+                "File %s could not be opened!!!\n", argv[ix]);
+                return -1;
+            }
+            ix++;
+        } else if (strcmp(argv[ix], "-i") == 0 && ifdev == NULL) {
+            /*
+             * Parse the interface
+             */
+            ix++;
+            ifdev = argv[ix];
+            ix++;
 
-		}
-		else {
-			fprintf(stderr, "Unknown or duplicate argument:%s\n", argv[ix]);
-			return -1;
-		}
-	}
-	return 0;
+        } else {
+            fprintf(stderr,
+            "Unknown or duplicate argument:%s\n", argv[ix]);
+            return -1;
+        }
+    }
+    return 0;
 }
 
+int main (int argc, char *argv[]) {
 
+    memset(rule_table, MAX_ENTRIES, sizeof(ipr_rule_t));
+    rule_cnt = 0;
 
-int main (int argc, char *argv[]){
+    if (!parse_args(argc, argv)) {
+        if (fh != NULL) {
+            fclose(fh);
+        }
+        exit(1);
+    }
 
-	memset(rule_table, MAX_ENTRIES, sizeof(ipr_rule_t) );
-	rule_cnt = 0;
+    if (!parse_file(fh)) {
+        if (fh != NULL) {
+            fclose(fh);
+        }
+        exit(1);
+    }
 
-	if (!parse_args(argc, argv)) {
-		fclose(fh);
-		exit(1);
-	}
-
-	if (!parse_file(fh)) {
-		fclose(fh);
-		exit(1);
-	}
-
-	return 0;
+    return 0;
 }
