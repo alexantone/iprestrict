1a3e8cd66891e7552969cfc6e5c834ee3042a07d
alex.antone <alex.antone@47f1c0aa-a667-11dd-9bad-6bc74a82fd44>
2008-10-30 11:53:55 +0000

    misc. refactoring TODO: parsing functionss refactorisation

--- trunk/parse.h
+++ trunk/parse.h
@@ -11,4 +11,10 @@
 #define PARSE_H_
 
 
+#define WHITESPACE_CHARS " \t"
+
+extern int parse_ip (char* token, int cline, int rule_cnt);
+extern int parse_range (char* token, int cline, int rule_cnt);
+extern int parse_subnet (char* token, int cline, int rule_cnt);
+
 #endif /* PARSE_H_ */
