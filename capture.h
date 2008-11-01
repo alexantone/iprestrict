80f4398ef70e0e3b91ddb4f5442775b1497568ec
dirty.douzin12 <dirty.douzin12@47f1c0aa-a667-11dd-9bad-6bc74a82fd44>
2008-11-01 10:40:34 +0000

    * Hide internal parsing functions

---
+++ capture.h
@@ -0,0 +1,14 @@
+/*
+ * -----------------------------------------------
+ * capture.h
+ *
+ *  Created on: Nov 1, 2008
+ *      Author: sorin
+ * -----------------------------------------------
+ */
+
+#ifndef CAPTURE_H_
+#define CAPTURE_H_
+
+
+#endif /* CAPTURE_H_ */
