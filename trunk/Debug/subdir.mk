f3f66bf32de9857285c9110a1b1e6cbeece527db
alex.antone <alex.antone@47f1c0aa-a667-11dd-9bad-6bc74a82fd44>
2008-11-02 19:10:42 +0000

    * Added makefiles

---
+++ trunk/Debug/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../capture.c \
+../iprestrict.c \
+../parse.c \
+../subinterface.c 
+
+OBJS += \
+./capture.o \
+./iprestrict.o \
+./parse.o \
+./subinterface.o 
+
+C_DEPS += \
+./capture.d \
+./iprestrict.d \
+./parse.d \
+./subinterface.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -DDEBUG -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
